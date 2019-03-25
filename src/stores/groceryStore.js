d6f293ec4240b1c1680585dcc5c3f9626902f794
Tal Bronfer <bronfer@gmail.com>
2019-03-25 19:15:20 +0200

    added refactored library, ruined a few tests

--- src/stores/groceryStore.js
+++ src/stores/groceryStore.js
@@ -18,7 +18,7 @@ class ShoppingList {
         let item = this.list.find(i => i.name === name)
         item.completed = !item.completed
     }
-    @action addItem = (name) => {
+    addItem = (name) => {
         const newItem = new Item(name)
         this.list.push(newItem)
     }
--- tests/exercises/ex-1.test.js
+++ tests/exercises/ex-1.test.js
@@ -2,18 +2,22 @@
 import { mount, render, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import store from '../../src/stores/groceryStore'
+import { ShoppingList, Item } from '../../src/stores/groceryStore';
 
 configure({ adapter: new Adapter() });
+
+let shoppingList;
 
 describe("exercise1", () => {
     it ('store should have an addItem function that adds a new Item to the stores list property', () => {
         const addItem = store.addItem
         addItem("test")
-        const test = store.list.find(i => i.name === "test")
-        expect(test).toBeTruthy()
+        const test = shoppingList.list.find(i => i.name === "test")
+        expect(test, "The item wasn't correctly added to the state. Make sure that it works.").toBeTruthy()
     })
     it ('addItem function should be a MobX action', () => {
         const addItem = store.addItem
-        expect(addItem.isMobxAction).toBeTruthy()
+        expect(addItem.isMobxActionm, "addItem is not a MobX function. Make sure it has the decorator syntax").toBeTruthy()
     })
+    
 })
