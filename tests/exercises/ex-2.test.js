e3534599b99c6c13a436e411ccc9038bb3bf0d49
Tal Bronfer <bronfer@gmail.com>
2019-03-31 15:32:41 +0300

    yalla

--- tests/exercises/ex-2.test.js
+++ tests/exercises/ex-2.test.js
@@ -17,7 +17,7 @@ describe("exercise2", () => {
     it('The location property should have a default value of "Super Sell"', () => {
         store.addItem("test")
         let test = store.list.find(i => i.name === "test")
-        expect(test.location, `Value of location is ${test.location}. Should be: "Super Sell 2"`).toBe("Super Sell")
+        expect(test.location, `Value of location is ${test.location}. Should be: "Super Sell"`).toBe("Super Sell2")
     })
 
 
